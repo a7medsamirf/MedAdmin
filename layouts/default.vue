0df58aba1769867ea25d06ebc6e16a9c8145b4df
ahmed samir <ahmed.samir.farid@gmail.com>
2022-01-03 15:25:35 +0200

    V 0.1

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,6 +6,7 @@
       :clipped="clipped"
       fixed
       app
+
     >
       <v-list>
         <v-list-item
@@ -14,6 +15,7 @@
           :to="item.to"
           router
           exact
+          color="primary"
         >
           <v-list-item-action>
             <v-icon>{{ item.icon }}</v-icon>
@@ -22,7 +24,39 @@
             <v-list-item-title v-text="item.title" />
           </v-list-item-content>
         </v-list-item>
+
       </v-list>
+      <nav-menu-section-title title="USER INTERFACE">USER INTERFACE</nav-menu-section-title>
+
+      <v-list>
+        <v-list-group
+          :value="true"
+          prepend-icon="mdi-account-circle"
+        >
+          <template v-slot:activator>
+            <v-list-item-title>Users</v-list-item-title>
+          </template>
+          <v-list-item
+            v-for="(User, i) in Users"
+            :key="i"
+            :to="User.to"
+            router
+            exact
+            color="primary"
+          >
+            <v-list-item-action>
+              <v-icon>{{ User.icon }}</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title v-text="User.title" />
+            </v-list-item-content>
+          </v-list-item>
+
+        </v-list-group>
+      </v-list>
+
+
+
     </v-navigation-drawer>
     <v-app-bar
       :clipped-left="clipped"
@@ -58,8 +92,10 @@
       </v-btn>
     </v-app-bar>
     <v-main>
-      <v-container>
-        <Nuxt />
+      <v-container fluid>
+        <v-slide-x-transition mode="out-in">
+          <Nuxt />
+        </v-slide-x-transition>
       </v-container>
     </v-main>
     <v-navigation-drawer
@@ -78,6 +114,7 @@
           <v-list-item-title>Switch drawer (click me)</v-list-item-title>
         </v-list-item>
       </v-list>
+
     </v-navigation-drawer>
     <v-footer
       :absolute="!fixed"
@@ -96,16 +133,15 @@ export default {
       drawer: false,
       fixed: false,
       items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
+        {icon: 'mdi-apps', title: 'Welcome', to: '/'},
+        {icon: 'mdi-calendar', title: 'Calendar', to: '/Calendar'},
+        {icon: 'mdi-account-arrow-left-outline', title: 'Inspire', to: '/inspire'},
+        {icon: 'mdi-chart-bubble', title: 'Inspire', to: '/inspire'},
+      ],
+      Users: [
+        {icon: 'mdi-circle-outline', title: 'list', to: '/user/List'},
+        {icon: 'mdi-circle-outline', title: 'View', to: '/user/View'},
+        {icon: 'mdi-circle-outline', title: 'Edit', to: '/user/Edit'},
       ],
       miniVariant: false,
       right: true,
